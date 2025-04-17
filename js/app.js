31faf8394a4fcf3e4fd4ebd58e6d98dccf1935ea
carloscMe <carloscarrillo.sprout@gmail.com>
2025-04-17 16:33:21 -0500

    funcionalidad ok

--- js/app.js
+++ js/app.js
@@ -1,27 +1,40 @@
 const imagen = document.querySelector(".img");
 const texto = document.querySelector(".texto");
-const si = document.querySelector(".si");
-const no = document.querySelector(".no");
+const btnSi = document.querySelector(".si");
+const btnNo = document.querySelector(".no");
 
 const imagenes = {   
-
      imagen1 : "./assets/gatoSi.jpg", 
      imagen2 : "./assets/gatoNo1.jpg",
      imagen3 :  "./assets/gatoNo2.jpg"
+};
+
+const {imagen1, imagen2, imagen3} = imagenes;
+
+const mostrar = (img, texto, pixel)=>{
+    imagen.src = img;
+    btnNo.innerText = `${texto}`;
+    btnSi.style.padding = `${pixel}px`;
+  removeEventListener("click", mostrar);
 }
 
-si.addEventListener("click", () =>{
-    imagen.src = imagenes.imagen1;
+btnSi.addEventListener("click", () =>{
+    imagen.src = imagen1;
     texto.innerText = "uwu :3";
-    si.style.display = "none";
-    no.style.display ="none";
+    btnSi.style.display = "none";
+    btnNo.style.display ="none";
 });
 
-no.addEventListener("click", ()=>{
-    imagen.src = imagenes.imagen3;
-    no.innerText = "ta bien";
-    si.style.padding = "20px";
-})
+btnNo.addEventListener("click", () =>{   // si algo se es que los eventos se comportan con desencadenamiento
+    mostrar(imagen1, "1", 10)
+    btnNo.addEventListener("click", () =>{
+    mostrar(imagen2 , "2", 20)
+    btnNo.addEventListener("click", () =>{
+    mostrar(imagen3 , "3", 30) 
+    btnNo.addEventListener("click", ()=>{
+    mostrar(imagen1 , "4", 40)
+    btnNo.addEventListener("click", ()=>{
+    mostrar(imagen2 , "5", 50) }); }); }); }); });
 
 
 
